f2a8bf51d7e1d556b8352d51913f1f5df286b220
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-01 21:31:17 -0500

    Filtering data Printing table

--- UFO-level-1/js/app.js
+++ UFO-level-1/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+let tbody = d3.select("tbody");
+let filterBtn = d3.select("#filter-btn");
+
+// Filter button click event handler
+filterBtn.on("click", function() {
+    d3.event.preventDefault();
+
+    // Filter data by entered date
+    let datetime = d3.select("#datetime").property("value");
+    filterData = tableData.filter(sighting => sighting.datetime == datetime);
+
+    // Clear table
+    tbody.html("");
+
+    // Fill table
+    filterData.forEach(function(sighting){
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
