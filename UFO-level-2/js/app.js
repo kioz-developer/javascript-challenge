850f5ba1314791f0c0b9798315af23062cb715d7
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-02 07:41:01 -0500

    Getting unique shapes from data to create html select options

--- UFO-level-2/js/app.js
+++ UFO-level-2/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Getting distinct shapes from data
+var shapes = tableData.map(sighting => sighting.shape);
+var uniqueShapes = shapes.filter((v, i, a) => a.indexOf(v) === i);
+
+
+let shapeFilter = d3.select("#shape");
+shapeFilter.append("option");
+
+// Filling shapes filter
+uniqueShapes.forEach(function(shape){
+    let row = shapeFilter.append("option");
+    row.attr("value", shape);
+    row.html(shape);
+});
