91f67d5d53837399bfed5e54aca710c5400889a0
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-02 08:03:33 -0500

    Applying custom function to all html select elements

--- UFO-level-2/js/app.js
+++ UFO-level-2/js/app.js
@@ -2,19 +2,25 @@
 var tableData = data;
 
 // Getting distinct values from data
-let shapes = tableData.map(sighting => sighting.shape);
 let states = tableData.map(sighting => sighting.state);
+let countries = tableData.map(sighting => sighting.country);
+let shapes = tableData.map(sighting => sighting.shape);
+let uniqueCountries = countries.filter((v, i, a) => a.indexOf(v) === i);
+let uniqueStates = states.filter((v, i, a) => a.indexOf(v) === i);
 let uniqueShapes = shapes.filter((v, i, a) => a.indexOf(v) === i);
-var uniqueState = states.filter((v, i, a) => a.indexOf(v) === i);
 
+// Creating html options to filters
+let stateFilter = d3.select("#state");
+let countryFilter = d3.select("#country");
 let shapeFilter = d3.select("#shape");
 
-// Filling shapes filter
+createOptions(uniqueStates, stateFilter);
+createOptions(uniqueCountries, countryFilter);
 createOptions(uniqueShapes, shapeFilter);
 
 // Create html options to select element
 function createOptions(dataList, selectElement) {
-    shapeFilter.append("option");
+    selectElement.append("option");
 
     dataList.forEach(function(value){
         let row = selectElement.append("option");
